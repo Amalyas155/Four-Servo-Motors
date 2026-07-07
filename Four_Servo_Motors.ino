e017f017096bdb222c368b7e10814a3e169bac2c
Amal yasser <amalyasser903@gmail.com>
2026-07-07 21:28:00 +0300

    Remove empty line at the beginning of the file

---
+++ Four_Servo_Motors.ino
@@ -0,0 +1,48 @@
+// C++ code
+//
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int pos = 0;
+
+void setup() {
+  servo1.attach(9);
+  servo2.attach(10);
+  servo3.attach(11);
+  servo4.attach(12);
+
+  unsigned long startTime = millis();
+
+  while (millis() - startTime < 2000) {
+    for (pos = 0; pos <= 180; pos++) {
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      delay(10);
+      if (millis() - startTime >= 2000) break;
+    }
+
+    for (pos = 180; pos >= 0; pos--) {
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      delay(10);
+      if (millis() - startTime >= 2000) break;
+    }
+  }
+
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+}
+
+void loop() {
+  // Motors stay at 90 degrees
+}
